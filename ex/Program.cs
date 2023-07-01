23ee66409060f891fdb1f76d2c393b0eefe83147
anonim <djumppp@mail.ru>
2023-07-01 13:23:04 +0300

    bulder ex

--- ex/Program.cs
+++ ex/Program.cs
@@ -1,3 +1,8 @@
-﻿// Console - обращаеться к консоле
+﻿// Console - Через Conosle мы обращаемся к системному терминалу
 // Write - что то выводит в окно терминала
+// dotnet new console - создает новый проект
+// dotnet run - запускает проект
+
+// Через Conosle мы обращаемся к системному терминалу
+
 Console.Write("Hello Artem Igorevich");
