c048a4b322dc042390cbc16fc511c31f68ebaeb3
anonim <djumppp@mail.ru>
2023-07-01 13:56:58 +0300

    new task

---
+++ ex2/Program.cs
@@ -0,0 +1,8 @@
+﻿// 1. Задача. Сложить два числа.
+
+Console.Write("Hello, enter your name: ");
+string username = Console.ReadLine();
+int num1 = 33;
+int num2 = 657456754;
+int sum = num1 + num2;
+Console.Write($"{username} sum {num1} and {num2} = {sum}");
